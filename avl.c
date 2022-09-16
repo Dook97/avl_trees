b7203fed3a81ef99d8c59fd1498a5adadc13823e
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2022-09-16 00:00:00 +0000

    fix avl_insert to allow inserting to empty tree

--- avl.c
+++ avl.c
@@ -19,7 +19,7 @@ typedef struct {
 
 /* --- INTERNAL FUNCTIONS ------------------------------------- */
 
-/* choose next node on the path to the node with given key according to the BST invariant */
+/* choose next node on the path to node with given key according to BST invariant */
 static avl_node_t **choose_son(uint32_t key, avl_node_t *node) {
 	return (key < node->key) ? &node->left_son : &node->right_son;
 }
@@ -36,7 +36,6 @@ static int avl_find_getaddr(avl_key_t key, avl_root_t *root, avl_node_t ***out) 
 		current_son  = choose_son(key, *current_node);
 	}
 	*out = current_node;
-
 	return *current_node != NULL && key == (*current_node)->key;
 }
 
@@ -46,22 +45,18 @@ static int avl_find_getaddr(avl_key_t key, avl_root_t *root, avl_node_t ***out) 
  * arguments are pointers to fathers' pointers to the nodes
  */
 static void replace_node(avl_node_t **replaced, avl_node_t **replacement) {
-	/* change father pointers in sons of replaced to point to replacement */
 	if ((*replaced)->left_son != NULL)
 		(*replaced)->left_son->father = *replacement;
 	if ((*replaced)->right_son != NULL)
 		(*replaced)->right_son->father = *replacement;
 
-	/* change father pointer of replacement to that of replaced */
 	(*replacement)->father = (*replaced)->father;
 
 	avl_node_t *temp = (*replacement)->right_son;
 
-	/* change son pointers of replacement to those of replaced */
 	(*replacement)->left_son = (*replaced)->left_son;
 	(*replacement)->right_son = (*replaced)->right_son;
 
-	/* change father's pointer from replaced to replacement */
 	*replaced = *replacement;
 
 	/* exchange pointer to replacement for pointer to it's right son in it's original father */
@@ -91,29 +86,30 @@ static avl_node_t **get_min_node(avl_node_t *root) {
  *  / \             / \
  * A   B           B   C
  * it is presumed that x and y are non-null
- * x,y represent nodes while A,B,C represent (possibly empty) subtrees
+ * x, y represent nodes while A, B, C represent (possibly empty) subtrees
+ * arguments are named according to the left part of the diagram
  */
-static void rotate(avl_node_t **father, int left_to_right) {
-	avl_node_t **ptr_to_son = left_to_right ? &(*father)->left_son : &(*father)->right_son;
-	avl_node_t *son = *ptr_to_son;
-	avl_node_t **Bnode = left_to_right ? &son->right_son : &son->left_son;
+static void rotate(avl_node_t **ynode, int left_to_right) {
+	avl_node_t **ptr_to_x = left_to_right ? &(*ynode)->left_son : &(*ynode)->right_son;
+	avl_node_t *xnode = *ptr_to_x;
+	avl_node_t **Bnode = left_to_right ? &xnode->right_son : &xnode->left_son;
 
 	/* make B son of y */
 	if (*Bnode != NULL)
-		(*Bnode)->father = *father;
-	*ptr_to_son = *Bnode;
+		(*Bnode)->father = *ynode;
+	*ptr_to_x = *Bnode;
 
 	/* move x to top */
-	son->father = (*father)->father;
-	*Bnode = *father;
-	*father = son;
+	xnode->father = (*ynode)->father;
+	*Bnode = *ynode;
+	*ynode = xnode;
 }
 
 /* --- PUBLIC FUNCTIONS --------------------------------------- */
 
 /* returns 1 if node with given key was found otherwise 0
- * out will point to node with given key if such exists
- * if it doesn't it will point to last node visited by the 'find' operation (use NULL to discard)
+ * out will point to node with given key if such exists (use out == NULL to discard)
+ * if it doesn't it will point to last node visited by the 'find' operation
  */
 int avl_find(avl_key_t key, avl_root_t *root, avl_node_t **out) {
 	avl_node_t **out_local;
@@ -132,8 +128,8 @@ int avl_insert(avl_node_t *new_node, avl_root_t *root) {
 		return 1;
 
 	new_node->father = father;
-	*choose_son(new_node->key, father) = new_node;
-
+	*((root->root_node == NULL) ? &root->root_node
+				    : choose_son(new_node->key, father)) = new_node;
 	return 0;
 }
 
@@ -162,8 +158,12 @@ int avl_delete(avl_key_t key, avl_root_t *root, avl_node_t **deleted) {
 
 /* --- DEBUG -------------------------------------------------- */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 void avl_enumerate(avl_node_t *root, int depth) {
-	if(!root) {
+	if (root == NULL) {
 		printf("%*c-\n", depth, ' ');
 		return;
 	}
@@ -173,18 +173,15 @@ void avl_enumerate(avl_node_t *root, int depth) {
 }
 
 int main() {
-	avl_node_t one, two, three, four, five, six;
-	avl_node_t *ptr_to_last;
-	two   = (avl_node_t){2,&four,&one,NULL};
-	four  = (avl_node_t){4,NULL,NULL,&two};
-	one   = (avl_node_t){1,&five,&three,&two};
-	five  = (avl_node_t){5,NULL,NULL,&one};
-	three = (avl_node_t){3,NULL,NULL,&one};
-	avl_root_t root = { .root_node = &two };
+	srandom(time(NULL));
 
-	avl_enumerate(root.root_node, 1);
-	printf("\nrotated\n\n");
-	rotate(&root.root_node, 0);
+	avl_root_t root = { .root_node = NULL };
+	avl_node_t nodes[5];
+	for (int i = 0; i < 5; ++i) {
+		nodes[i] = (avl_node_t){random() % 100};
+		avl_insert(nodes + i, &root);
+	}
+
 	avl_enumerate(root.root_node, 1);
 }
 
